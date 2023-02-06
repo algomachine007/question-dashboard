0e17607d80101a06cc3564e964d3dceb5095b8e8
algomachine007 <amaben202phantom@gmail.com>
2023-02-06 15:11:18 +0100

    project setup

---
+++ src/data/questions.js
@@ -0,0 +1,9 @@
+export const questions = [{ question: "Why do you loke our service ?" }];
+
+export const result = {
+  q1q2: "Component 1",
+  q2q3: "Component 2",
+  q1q3: "Component 3",
+  q1: "No Results found for this entry",
+  q2: "No Results found for this entry ",
+};
