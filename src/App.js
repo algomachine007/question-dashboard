9eb60551088f671eae396fc358f506c6150c33e3
algomachine007 <amaben202phantom@gmail.com>
2023-02-07 09:04:36 +0100

    completing the major functionality

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import { useContext, useState } from "react";
 import "./App.css";
 import { resultContext } from "./context/resultContext";
-import { questions } from "./data/questions";
+import { questions, result } from "./data/questions";
 
 function App() {
   const { state, updateState } = useContext(resultContext);
   const [step, setStep] = useState(0);
-  console.log("STATE", state);
 
   const [selectedAnswer, setSelectedAnswer] = useState("");
-
-  console.log("selected", selectedAnswer);
 
   const activeStep = questions[step];
 
@@ -22,54 +19,87 @@ function App() {
       case "prev":
         return setStep((p) => (p > 0 ? p - 1 : p));
 
+      case "restart":
+        return setStep(0);
+
       default:
         return null;
     }
   };
 
+  const computeResults = () => {
+    const responses = [...state];
+
+    const transformResponses = responses.map((response, index) => {
+      if (response) {
+        return {
+          [`q${index + 1}`]: response === "yes" ? "yes" : "no",
+        };
+      }
+      return null;
+    });
+
+    return transformResponses.reduce((acc, currentValue) => {
+      Object.entries(currentValue).map(([key, value]) => {
+        if (value === "yes") {
+          return (acc += key);
+        }
+      });
+      return acc;
+    }, "");
+  };
+
+  console.log(computeResults());
+
+  const Component = result[computeResults()];
+  console.log(Component);
+
   return (
-    <div className="App">
-      <div>
-        <h2>
-          {activeStep.id}- {activeStep.question}
-        </h2>
-      </div>
+    <>
+      {!Component && (
+        <div className="App">
+          <div>
+            <h2>
+              {activeStep.id}- {activeStep.question}
+            </h2>
+          </div>
+          {activeStep.answers.map((answer) => (
+            <>
+              <input
+                type="radio"
+                value={String(answer)}
+                onChange={(e) => setSelectedAnswer(e.target.value)}
+                checked={answer === selectedAnswer}
+              />
+              <label>{answer}</label>
+            </>
+          ))}
+          <div className="btn">
+            <button onClick={() => updateState(selectedAnswer)} type="submit">
+              Submit
+            </button>
 
-      <form
-      // onSubmit={(e) => {
-      //   e.preventDefault();
-      //   updateState(selectedAnswer);
-      // }}
-      >
-        {activeStep.answers.map((answer) => (
-          <>
-            <input
-              type="radio"
-              value={String(answer)}
-              onChange={(e) => setSelectedAnswer(e.target.value)}
-              checked={answer === selectedAnswer}
-            />
-            <label>{answer}</label>
-          </>
-        ))}
-      </form>
+            <button onClick={() => handleStep("prev")} disabled={step <= 0}>
+              Prev
+            </button>
 
-      <button onClick={() => updateState(selectedAnswer)} type="submit">
-        Submit
-      </button>
+            <button
+              onClick={() => handleStep("next")}
+              disabled={step === Number(questions.length) - 1}
+            >
+              Next
+            </button>
 
-      <button onClick={() => handleStep("prev")} disabled={step <= 0}>
-        Prev
-      </button>
+            {step === Number(questions.length) - 1 && (
+              <button onClick={() => handleStep("restart")}>Restart</button>
+            )}
+          </div>
+          {state && state.map((e) => <div>{e}</div>)}
+        </div>
+      )}
 
-      <button
-        onClick={() => handleStep("next")}
-        disabled={step === Number(questions.length) - 1}
-      >
-        Next
-      </button>
-      {state && state.map((e) => <div>{e}</div>)}
-    </div>
+      {Component && <div className="result">{Component}</div>}
+    </>
   );
 }
 
--- src/data/questions.js
+++ src/data/questions.js
@@ -6,12 +6,12 @@ export const questions = [
   },
   {
     id: "2",
-    question: "Why do you like our service ?",
+    question: "Do you prefer online communication?",
     answers: ["yes", "no", "skip"],
   },
   {
     id: "3",
-    question: "Why do you like our service ?",
+    question: "How can we make our service better ?",
     answers: ["yes", "no", "skip"],
   },
 ];
@@ -20,6 +20,7 @@ export const result = {
   q1q2: "Component 1 for Q1Q2",
   q2q3: "Component 2 for Q2Q3",
   q1q3: "Component 3 Q1Q3",
+  q1q2q3: "You selected positive feedbacks for Q1, Q2, Q3",
   q1: "No Results found for this entry Q1",
   q2: "No Results found for this entry. Q2",
 };
